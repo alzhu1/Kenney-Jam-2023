70da17c8d4addd0ad455302a5b9f25572d2cb5c7
alzhu1 <er.and.alex@gmail.com>
2023-07-21 22:19:30 -0700

    Add restart/back to title buttons, plus levels 4a and 4b

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,29 +6,51 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour {
     public static LevelManager instance = null;
 
+    private bool loading;
+
     void Awake() {
         if (instance == null) {
             instance = this;
             DontDestroyOnLoad(gameObject);
         } else {
+            instance.loading = false;
             Destroy(gameObject);
             return;
         }
     }
 
+    void Update() {
+        if (loading) {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R)) {
+            RestartLevel();
+        } else if (Input.GetKeyDown(KeyCode.Escape)) {
+            LoadTitle();
+        }
+    }
+
     public void LoadLevel(int index) {
+        if (loading) {
+            return;
+        }
+
         if (index >= SceneManager.sceneCountInBuildSettings) {
             LoadTitle();
         } else {
             SceneManager.LoadScene(index);
         }
+        loading = true;
     }
 
     public void RestartLevel() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        loading = true;
     }
 
     public void LoadTitle() {
         SceneManager.LoadScene(0);
+        loading = true;
     }
 }
