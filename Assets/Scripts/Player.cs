4296952ae7a02f0e0ebfeddd24503d741dc9755e
alzhu1 <er.and.alex@gmail.com>
2023-07-21 18:20:13 -0700

    Update collision checking, prevent growing if in enclosed space

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ using Cinemachine;
 public class Player : MonoBehaviour {
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpSpeed;
-    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform[] directionChecks;
     [SerializeField] private LayerMask groundLayer;
 
     [SerializeField] private float changeSizeTime;
@@ -15,6 +15,7 @@ public class Player : MonoBehaviour {
     [SerializeField] private CinemachineVirtualCamera[] cams;
 
     private Animator animator;
+    private BoxCollider2D box;
     private Rigidbody2D rb;
 
     private float horizontal;
@@ -26,6 +27,7 @@ public class Player : MonoBehaviour {
         
     void Awake() {
         animator = GetComponent<Animator>();
+        box = GetComponent<BoxCollider2D>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -66,8 +68,9 @@ public class Player : MonoBehaviour {
         Vector2 currVelocity = rb.velocity;
         currVelocity.x = horizontal * moveSpeed * Time.fixedDeltaTime;
 
-        // TODO: Change ground check to use box instead of circle
-        grounded = Physics2D.OverlapCircle(groundCheck.position, 0.05f, groundLayer);
+        Vector2 cornerOffset = new Vector2(box.bounds.extents.x, 0.05f);
+        Vector2 pos = directionChecks[0].position;
+        grounded = Physics2D.OverlapArea(pos + cornerOffset, pos - cornerOffset, groundLayer);
 
         if (grounded && shouldJump) {
             // TODO: Probably better to just index into an array of predefined jump velocities, easier to test
@@ -79,6 +82,18 @@ public class Player : MonoBehaviour {
     }
 
     IEnumerator ChangeSize(bool grow) {
+        if (grow) {
+            bool[] checks = new bool[directionChecks.Length];
+            for (int i = 0; i < directionChecks.Length; i++) {
+                Vector3 futurePos = directionChecks[i].localPosition * 2 + transform.position;
+                checks[i] = Physics2D.OverlapCircle(futurePos, 0.05f, groundLayer);
+
+                if (i >= 2 && checks[i] && checks[i - 2]) {
+                    yield break;
+                }
+            }
+        }
+
         changingSize = true;
 
         Vector3 localScale = transform.localScale;
@@ -118,4 +133,12 @@ public class Player : MonoBehaviour {
         sizeIndex = nextIndex;
         changingSize = false;
     }
+
+    // void OnDrawGizmos() {
+    //     Gizmos.color = Color.red;
+    //     // Gizmos.DrawSphere(transform.position, temp);
+    //     Vector3 extents = box.bounds.extents;
+    //     Vector3 pos = (Vector3)box.offset + transform.position;
+    //     Gizmos.DrawLine(box.bounds.center+ new Vector3(extents.x, extents.y) * 2, box.bounds.center - new Vector3(extents.x, extents.y) * 2);
+    // }
 }
